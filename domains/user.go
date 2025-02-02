c216a311cda3ed403901048278625b971dbb06ce
faishalshidqi <rshidqi6@gmail.com>
2025-02-02 14:18:57 +0700

    added user domain & user repository

--- domains/user.go
+++ domains/user.go
@@ -1,1 +1,21 @@
 package domains
+
+import (
+	"context"
+	"time"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at" time_format:"01-01-2001 15:04:05"`
+}
+
+type UserRepository interface {
+	Add(ctx context.Context, user *User) error
+	Fetch(ctx context.Context) ([]*User, error)
+	GetByEmail(ctx context.Context, email string) (*User, error)
+	GetByID(ctx context.Context, id string) (*User, error)
+}
