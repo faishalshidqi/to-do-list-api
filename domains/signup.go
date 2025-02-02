aa3e0b64bd163e570c7c076ee362ee14ed29cf9c
faishalshidqi <rshidqi6@gmail.com>
2025-02-02 14:24:44 +0700

    added signup domain & signup use case interface

--- domains/signup.go
+++ domains/signup.go
@@ -1,1 +1,16 @@
 package domains
+
+import "context"
+
+type UserSignUp struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserSignUpUsecase interface {
+	Create(ctx context.Context, user *User) error
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+	CreateAccessToken(user *User, secret string, expiry int) (string, error)
+	CreateRefreshToken(user *User, secret string, expiry int) (string, error)
+}
