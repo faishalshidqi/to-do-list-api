66b46bbb8e1fdade219a6513c78030259add8a28
faishalshidqi <rshidqi6@gmail.com>
2025-02-04 17:46:45 +0700

    added login router

---
+++ interfaces/http/api/routes/loginRoute.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+	"todo-list-api/applications/usecase"
+	"todo-list-api/commons/bootstrap"
+	"todo-list-api/domains"
+	"todo-list-api/infrastructures/mongo"
+	"todo-list-api/infrastructures/repository"
+	"todo-list-api/interfaces/http/api/controllers"
+)
+
+func NewLoginRouter(env *bootstrap.Env, timeout time.Duration, db mongo.Database, group *gin.RouterGroup) {
+	userRepository := repository.NewUserRepository(db, domains.UserCollectionName)
+	loginController := controllers.LoginController{
+		LoginUsecase: usecase.NewLoginUsecase(userRepository, timeout),
+		Env:          env,
+	}
+	group.POST("/api/auth/login", loginController.Login)
+}
