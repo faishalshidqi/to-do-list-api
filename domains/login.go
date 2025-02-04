5c29e641a7f19c73b3cfaf51b4468a27e9259f9f
faishalshidqi <rshidqi6@gmail.com>
2025-02-04 17:40:48 +0700

    added message field to login response struct

--- domains/login.go
+++ domains/login.go
@@ -8,6 +8,7 @@ type LoginRequest struct {
 }
 
 type LoginResponse struct {
+	Message      string `json:"message"`
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 }
