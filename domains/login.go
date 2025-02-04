5a5ae34778cb0a463cd9a494bbf4c456ce20deba
faishalshidqi <rshidqi6@gmail.com>
2025-02-04 15:40:15 +0700

    updated CreateAccessToken and CreateRefreshToken signature

--- domains/login.go
+++ domains/login.go
@@ -14,6 +14,6 @@ type LoginResponse struct {
 
 type LoginUsecase interface {
 	GetUserByEmail(c context.Context, email string) (User, error)
-	CreateAccessToken(c context.Context, secret string, expiry int) (accessToken string, err error)
-	CreateRefreshToken(c context.Context, secret string, expiry int) (refreshToken string, err error)
+	CreateAccessToken(user *User, secret string, expiry int) (accessToken string, err error)
+	CreateRefreshToken(user *User, secret string, expiry int) (refreshToken string, err error)
 }
