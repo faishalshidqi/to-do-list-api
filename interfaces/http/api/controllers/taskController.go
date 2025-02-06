1547dc0dd6e1efb7dd194d5ed32612a796a2d04e
faishalshidqi <rshidqi6@gmail.com>
2025-02-06 17:14:03 +0700

    added get tasks by owner controller

--- interfaces/http/api/controllers/taskController.go
+++ interfaces/http/api/controllers/taskController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
+	"strconv"
 	"time"
 	"todo-list-api/commons/bootstrap"
 	"todo-list-api/commons/tokenize"
@@ -70,6 +71,59 @@ func (tc *TaskController) Create(c *gin.Context) {
 		Message: "Task created successfully",
 		Data: domains.AddTaskResponseData{
 			ID: task.ID,
+		},
+	})
+}
+
+func (tc *TaskController) GetByOwner(c *gin.Context) {
+	token, err := tokenize.GetBearerToken(c.Request.Header)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, domains.ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+	userId, err := tokenize.ValidateJWT(token, tc.Env.AccessTokenKey)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, domains.ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	page := c.DefaultQuery("page", "0")
+	size := c.DefaultQuery("size", "1")
+	convPage, err := strconv.Atoi(page)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domains.ErrorResponse{
+			Message: "Invalid page value",
+		})
+		return
+	}
+	convSize, err := strconv.Atoi(size)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domains.ErrorResponse{
+			Message: "Invalid size value",
+		})
+		return
+	}
+	tasks, err := tc.TaskUsecase.FetchByOwner(c, userId, convPage, convSize)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domains.ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+	if tasks == nil {
+		c.JSON(http.StatusNotFound, domains.ErrorResponse{
+			Message: "Task not found",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, domains.GetTaskResponse{
+		Message: "Successfully fetched tasks",
+		Data: domains.GetTaskResponseData{
+			Tasks: tasks,
 		},
 	})
 }
