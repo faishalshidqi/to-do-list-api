df5d5aca53ac72fc577b18e2131568a48420b3fb
faishalshidqi <rshidqi6@gmail.com>
2025-02-06 16:24:30 +0700

    updated POST /api/tasks docs

--- interfaces/http/api/controllers/taskController.go
+++ interfaces/http/api/controllers/taskController.go
@@ -15,6 +15,22 @@ type TaskController struct {
 	Env         *bootstrap.Env
 }
 
+// Create AddTask godoc
+//
+//	@Summary		Add A New Task To DB
+//	@Description	Add A New Task
+//	@Tags			tasks
+//	@Accept			json
+//	@Produce		json
+//	@Param			title		body		string	true	"task's title"
+//	@Param			password	body		string	true	"task's description"
+//	@Param			isCompleted	body		bool	true	"whether the task is completed"
+//
+//	@Success		201			{object}	domains.AddTaskResponse
+//	@Failure		400			{object}	domains.ErrorResponse
+//	@Failure		401			{object}	domains.ErrorResponse
+//	@Failure		500			{object}	domains.ErrorResponse
+//	@Router			/api/tasks [post]
 func (tc *TaskController) Create(c *gin.Context) {
 	token, err := tokenize.GetBearerToken(c.Request.Header)
 	if err != nil {
@@ -50,9 +66,9 @@ func (tc *TaskController) Create(c *gin.Context) {
 		})
 		return
 	}
-	c.JSON(http.StatusCreated, domains.TaskResponse{
+	c.JSON(http.StatusCreated, domains.AddTaskResponse{
 		Message: "Task created successfully",
-		Data: domains.TaskResponseData{
+		Data: domains.AddTaskResponseData{
 			ID: task.ID,
 		},
 	})
