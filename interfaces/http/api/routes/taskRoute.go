79c0ee3988c7b7f7a1d495a4685c350e08460679
faishalshidqi <rshidqi6@gmail.com>
2025-02-07 14:46:36 +0700

    added update task by id route at PUT /api/tasks/{id}

--- interfaces/http/api/routes/taskRoute.go
+++ interfaces/http/api/routes/taskRoute.go
@@ -20,4 +20,5 @@ func NewTaskRouter(env *bootstrap.Env, timeout time.Duration, db mongo.Database,
 	group.POST("/api/tasks", taskController.Create)
 	group.GET("/api/tasks", taskController.GetByOwner)
 	group.GET("/api/tasks/:id", taskController.GetById)
+	group.PUT("/api/tasks/:id", taskController.Update)
 }
