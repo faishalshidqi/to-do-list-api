e6745c814874b6fcb278441c3c91978ef7cf3018
faishalshidqi <rshidqi6@gmail.com>
2025-02-08 14:39:42 +0700

    updated user struct

--- domains/user.go
+++ domains/user.go
@@ -11,7 +11,7 @@ const (
 )
 
 type User struct {
-	ID        primitive.ObjectID `json:"id" bson:"id"`
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	Name      string             `json:"name" bson:"name"`
 	Email     string             `json:"email" bson:"email"`
 	Password  string             `json:"password" bson:"password"`
